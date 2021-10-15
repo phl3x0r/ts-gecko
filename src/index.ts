f6de6f33eee294456f4d6dabf646c95803bf642e
Niklas Hegnelt <wispster@gmail.com>
2021-10-15 22:47:52 +0200

    add rxjs example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "isomorphic-fetch";
+import { from, switchMap } from "rxjs";
 import { Api } from "./api.js";
 
 const client = new Api();
@@ -8,3 +9,8 @@ client.coins
   .categoriesList()
   .then((res) => res.json())
   .then((categories) => console.log(categories));
+
+// rxjs way
+from(client.coins.categoriesList())
+  .pipe(switchMap((res) => res.json()))
+  .subscribe(console.log);
